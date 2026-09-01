514ee82c06125895d3a4adfa9709c051508741ca
Amit Raj <navodaya2152@gmail.com>
2026-09-01 11:47:17 +0530

    Support Science practice and CBT routes

--- js/home-routing.js
+++ js/home-routing.js
@@ -5,6 +5,15 @@
       const view=new URLSearchParams(location.search).get('view');
       if(view==='science' && typeof window.openChapter==='function'){
         window.openChapter(1,0);
+        return;
+      }
+      if(view==='science-practice' && window.FullScienceTest?.start){
+        window.FullScienceTest.start(4);
+        return;
+      }
+      if(view==='science-cbt' && window.ScienceCBT?.open){
+        window.ScienceCBT.open();
+        return;
       }
     }catch(_){/* keep homepage usable if routing fails */}
   }
