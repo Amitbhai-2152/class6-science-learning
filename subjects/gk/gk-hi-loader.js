ba0b860fa6c782c34e84effb36da21bb808107d2
Amit Raj <navodaya2152@gmail.com>
2026-09-01 16:53:50 +0530

    Fix Hindi GK loader to preserve expanded lessons

--- subjects/gk/gk-hi-loader.js
+++ subjects/gk/gk-hi-loader.js
@@ -1,1 +1,1 @@
-(function(){'use strict';if(!Array.isArray(window.GK_HI_TOPICS)||!Array.isArray(window.REASONING_HI))return;window.GK_HI_TOPICS.forEach(function(t,i){var base=window.GK_TOPICS&&window.GK_TOPICS[i];if(base){t.lessons=base.lessons;t.icon=base.icon;}});window.GK_HI_READY=true;})();
+(function(){'use strict';if(!Array.isArray(window.GK_HI_TOPICS))return;window.GK_HI_TOPICS.forEach(function(t,i){var base=window.GK_TOPICS&&window.GK_TOPICS[i];if(base){t.icon=base.icon;}});window.GK_HI_READY=true;})();
